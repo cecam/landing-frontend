fd6aa0e05d40db2809b399cfc618876650e5dba1
Celso Cardenas <cardenascelso9@gmail.com>
2023-07-14 00:57:57 -0600

    sistema de autenticación terminado

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,77 @@
 'use client'
 
-import Form from "@/components/Login/Form";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import RegisterForm from "@/components/Login/RegisterForm";
+import LoginForm from "@/components/Login/LoginForm";
+
+import supabase from "@/utils/supabaseClient";
+import useForm from "@/hooks/useForm";
+
+interface IEmailPassword {
+    email: string,
+    password: string
+}
 
 const LoginPage = () => {
+    const [ formStatus, setFormStatus  ] = useState('login');
+    const router = useRouter();
+    const [ formState, handleChange ] = useForm({
+        email: '',
+        password: ''
+    });
+    const { email, password } = formState;
+
+    const signUpWithEmail = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        try {
+            const resp = await supabase.auth.signUp({email, password})
+            if(resp.error) throw resp.error
+
+            const userID = resp.data.user?.id;
+            router.push('/');
+        } catch (error) {
+            throw error
+        }
+    }
+
+    const signInWithEmail = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        try {
+            const resp = await supabase.auth.signUp({email, password})
+            if(resp.error) throw resp.error
+
+            const userID = resp.data.user?.id;
+            router.push('/');
+        } catch (error) {
+            throw error
+        }
+    }
+
     return (
-        <Form />
+        <>
+            {formStatus === 'login'
+                ?   <LoginForm 
+                        handleChange={handleChange} 
+                        email={email}
+                        password={password}    
+                        onSubmit={signInWithEmail}
+                        setFormStatus={setFormStatus}
+                    />
+
+                :   <RegisterForm 
+                        handleChange={handleChange} 
+                        email={email}
+                        password={password}    
+                        onSubmit={signUpWithEmail}
+                        setFormStatus={setFormStatus}
+                    />
+
+            }
+        </>
     )
 }
 
---
+++ components/Login/LoginForm.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { FC, ChangeEvent, Dispatch, SetStateAction } from "react";
+import Input from "../Input";
+
+interface IForm {
+    handleChange:(event: ChangeEvent<HTMLInputElement>) => void,
+    onSubmit:(e:any) => Promise<void>,
+    setFormStatus:Dispatch<SetStateAction<string>>,
+    email:string,
+    password:string
+}
+
+const LoginForm:FC<IForm> = ({handleChange, onSubmit, setFormStatus, email, password}) => {
+
+    return(
+        <form 
+            className="py-8 px-10 bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100
+                flex flex-col justify-evenly min-h-[380px] min-w-[480px] text-[#333]"
+            onSubmit={onSubmit}
+        >
+            <h1 className="text-3xl text-[#333]">Iniciar sesión</h1>
+            <Input 
+                id="email"
+                name="email"
+                type="text"
+                labelText="Correo electrónico"
+                placeholder="******"
+                onChange={handleChange}
+                value={email}
+            />
+            <Input 
+                id="password"
+                name="password"
+                type="password"
+                labelText="Contraseña"
+                placeholder="******"
+                onChange={handleChange}
+                value={password}
+            />
+
+            <button 
+                type="submit"
+                className="rounded-lg border uppercase border-orange-500 text-orange-500 transition-all duration-100 hover:border-2 hover:font-bold focus:text-white focus:bg-orange-300 py-3"
+            >
+                Entrar
+            </button>
+            <button onClick={() => setFormStatus('signup')}>ya tengo una cuenta</button>
+        </form>
+    )
+}
+
+export default LoginForm;
